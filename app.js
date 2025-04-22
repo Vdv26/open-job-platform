a8db09f9e9d61299b691139e0417889df3c82818
Vdv26 <vaishnavdv26@gmail.com>
2025-04-22 08:34:58 +0530

    app.js

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.set('view engine', 'ejs');
 app.use(jobRoutes);
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
